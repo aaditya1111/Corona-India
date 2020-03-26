bc9b31e66ac9d1beae3a6ffd819911d0887fe641
Vinit Shahdeo <vinit.shahdeo@getpostman.com>
2020-03-26 17:49:20 +0530

    updated the developer's message

--- src/App.js
+++ src/App.js
@@ -139,12 +139,12 @@ export default class App extends React.Component {
             <strong>Stay Home, Stay Safe</strong>
           </Alert.Heading>
           <p>
-            <strong>COVID-19</strong> or as the plebs call it, the coronavirus
+            <strong>COVID-19</strong> or as the plebs call it, the <strong>coronavirus </strong> 
             has restricted a lot of us to our homes. You can choose to while
-            away your time playing sock puppet or you could utilize this break
-            to ‘Learn’ something new. You know you’ve said this a lot to
-            yourself - “I want to Learn [X] but I can’t seem to find the time.”
-            This is time to learn something new. Turn Your Self-Isolation into
+            away your time sleeping; doing nothing or you could utilize this break
+            to <strong>‘Learn’</strong> something new. You know you’ve said this a lot to
+            yourself - “I want to Learn 'X' but I can’t seem to find the time.”
+            This is the time; Turn Your Self-Isolation into
             Self-Improvement!
           </p>
           <hr />
